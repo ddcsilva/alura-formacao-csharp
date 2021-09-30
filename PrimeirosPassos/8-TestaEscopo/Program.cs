87a5e9d2baeeac1e3c231e716af7be7ef8346a0b
Danilo Silva <danilo.silva@msn.com>
2021-09-29 22:16:52 -0300

    Projeto Testa Escopo

---
+++ PrimeirosPassos/8-TestaEscopo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _8_TestaEscopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando Condicionais");
+
+            int idade = 20;
+            int quantidadePessoas = 3;
+            bool acompanhado;
+
+            if (quantidadePessoas >= 2)
+            {
+                acompanhado = true;
+            }
+            else
+            {
+                acompanhado = false;
+            }
+
+            if (idade >= 18 || acompanhado)
+            {
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                Console.WriteLine("infelizmente você não pode entrar");
+            }
+        }
+    }
+}
