f9ce3c232364fecab2fe082efa824cda9878464f
Danilo Silva <danilo.silva@msn.com>
2021-09-29 22:52:46 -0300

    Projeto Laço de Repetição For 2

---
+++ PrimeirosPassos/12-LacoDeRepeticaoFor2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _12_LacoDeRepeticaoFor2
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                for (int contador = 0; contador <= 10; contador++)
+                {
+                    Console.Write(multiplicador + " * " + contador + " = " + multiplicador * contador);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
