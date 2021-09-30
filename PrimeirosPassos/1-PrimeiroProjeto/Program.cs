b77603015b87970b6de77d196c4c6ae8abc1df67
Danilo Silva <danilo.silva@msn.com>
2021-09-29 21:28:50 -0300

    Primeiro Projeto

---
+++ PrimeirosPassos/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo. Projeto no Visual Studio!");
+        }
+    }
+}
