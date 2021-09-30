f76f84a2c0a688c475f069d9d5fc93a39b2d2813
Danilo Silva <danilo.silva@msn.com>
2021-09-29 22:54:10 -0300

    Projeto Laço de Repetição 3

---
+++ PrimeirosPassos/13-LacoDeRepeticaoFor3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _13_LacoDeRepeticaoFor3
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            for (int linha = 0; linha < 10; linha++)
+            {
+                for (int coluna = 0; coluna < 10; coluna++)
+                {
+                    if (coluna > linha)
+                    {
+                        break;
+                    }
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
