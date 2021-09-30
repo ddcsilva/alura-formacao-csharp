277e75808c6af5df563ed3154445c257e5eb7a9a
Danilo Silva <danilo.silva@msn.com>
2021-09-29 22:50:33 -0300

    Projeto Laço de Repetição For

---
+++ PrimeirosPassos/11-LacoDeRepeticaoFor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _11_LacoDeRepeticaoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int contador = 0; contador <= 10; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+        }
+    }
+}
