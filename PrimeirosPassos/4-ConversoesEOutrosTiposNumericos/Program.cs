15a553aea66fd8bf3fe6260b5f1de813e455a047
Danilo Silva <danilo.silva@msn.com>
2021-09-29 21:56:55 -0300

    Projeto Conversões e Outros Tipos Numéricos

---
+++ PrimeirosPassos/4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pontoFlutuante = 3.14f;
+            Console.WriteLine(pontoFlutuante);
+
+            double salario = 1270.50;
+            int valor = (int)salario;
+            Console.WriteLine(valor);
+
+            double valor1 = 0.2;
+            double valor2 = 0.1;
+            double total = valor1 + valor2;
+
+            Console.WriteLine(total);
+        }
+    }
+}
