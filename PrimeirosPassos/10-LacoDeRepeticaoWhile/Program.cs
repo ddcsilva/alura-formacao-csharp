e668ad1e3ee729a0a958b6c5f8e144bba0b57ef3
Danilo Silva <danilo.silva@msn.com>
2021-09-29 22:49:26 -0300

    Projeto Laço de Repetição While

---
+++ PrimeirosPassos/10-LacoDeRepeticaoWhile/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _10_LacoDeRepeticaoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador = contador + 1;
+            }
+        }
+    }
+}
