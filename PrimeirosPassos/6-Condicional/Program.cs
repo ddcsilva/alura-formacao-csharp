29fdcaa83721c6cd0f51021942072fd615ddcb72
Danilo Silva <danilo.silva@msn.com>
2021-09-29 22:09:35 -0300

    Projeto Condicional

---
+++ PrimeirosPassos/6-Condicional/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _6_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 20;
+            int quantidadePessoas = 3;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você tem mais que 18 anos");
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("voce não tem 18, mas pode entrar, pois está acompanhado");
+
+                }
+                else
+                {
+                    Console.WriteLine("infelizmente você não pode entrar");
+                }
+            }
+        }
+    }
+}
