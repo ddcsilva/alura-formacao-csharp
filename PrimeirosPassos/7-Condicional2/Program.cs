969c0fd9318aa2e19e08260e2fec30235a19ed71
Danilo Silva <danilo.silva@msn.com>
2021-09-29 22:11:59 -0300

    Projeto Condicional 2

---
+++ PrimeirosPassos/7-Condicional2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _7_Condicional2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando Condicionais");
+
+            int idade = 16;
+            int quantidadePessoas = 3;
+            bool acompanhado = quantidadePessoas >= 2;
+
+            if (idade >= 18 && acompanhado)
+            {
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente voce não pode entrar");
+            }
+        }
+    }
+}
