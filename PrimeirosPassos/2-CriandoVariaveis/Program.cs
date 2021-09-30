fb158ced0c7533628f884e14103ad78848760242
Danilo Silva <danilo.silva@msn.com>
2021-09-29 21:50:38 -0300

    Projeto Criando Variávies

---
+++ PrimeirosPassos/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 31;
+
+            Console.WriteLine("A idade é " + idade + ", parabéns!");
+        }
+    }
+}
