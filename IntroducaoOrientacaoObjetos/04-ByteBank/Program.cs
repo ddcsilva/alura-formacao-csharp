87333fce6ba6e72b1fd75041744f918651bbc700
Danilo Silva <danilo.silva@msn.com>
2021-09-30 22:43:42 -0300

    Projeto: ByteBank - Nossa Primeira Função

---
+++ IntroducaoOrientacaoObjetos/04-ByteBank/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoDanilo = new ContaCorrente();
+            contaDoDanilo.titular = "Danilo";
+
+            Console.WriteLine(contaDoDanilo.saldo);
+            contaDoDanilo.Sacar(50);
+            Console.WriteLine(contaDoDanilo.saldo);
+
+            Console.WriteLine(contaDoDanilo.Sacar(500));
+        }
+    }
+}
