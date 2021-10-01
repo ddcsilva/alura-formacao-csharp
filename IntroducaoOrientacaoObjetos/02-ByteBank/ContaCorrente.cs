6ae549752e7bf87699a7022aa27dd0bb74f70597
Danilo Silva <danilo.silva@msn.com>
2021-09-30 21:48:59 -0300

    Projeto: ByteBank - Valores Padrões

---
+++ IntroducaoOrientacaoObjetos/02-ByteBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_ByteBank
+{
+    class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+    }
+}
---
+++ IntroducaoOrientacaoObjetos/02-ByteBank/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _02_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoDanilo = new ContaCorrente();
+            contaDoDanilo.titular = "Danilo";
+
+            Console.WriteLine(contaDoDanilo.titular);
+            Console.WriteLine("Agência: " + contaDoDanilo.agencia);
+            Console.WriteLine("Número: " + contaDoDanilo.numero);
+            Console.WriteLine("Saldo: " + contaDoDanilo.saldo);
+        }
+    }
+}
