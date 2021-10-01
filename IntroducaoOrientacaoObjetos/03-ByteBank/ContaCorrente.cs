5d2ff1fbbc12eb4a588b3b7f2646a62063aabd8b
Danilo Silva <danilo.silva@msn.com>
2021-09-30 22:31:07 -0300

    Projeto: ByteBank - Tipos de Referência e Tipos de Valor

---
+++ IntroducaoOrientacaoObjetos/03-ByteBank/ContaCorrente.cs
@@ -0,0 +1,10 @@
+﻿namespace _03_ByteBank
+{
+    class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+    }
+}
---
+++ IntroducaoOrientacaoObjetos/03-ByteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoDanilo = new ContaCorrente();
+            contaDoDanilo.titular = "Danilo";
+            contaDoDanilo.agencia = 863;
+            contaDoDanilo.numero = 863452;
+
+            ContaCorrente contaDoDaniloSilva = new ContaCorrente();
+            contaDoDaniloSilva.titular = "Danilo";
+            contaDoDaniloSilva.agencia = 863;
+            contaDoDaniloSilva.numero = 863452;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDoDanilo == contaDoDaniloSilva));
+
+            int idade = 30;
+            int idade2 = 30;
+
+            Console.WriteLine("Igualdade de tipo de valor: " + (idade == idade2));
+
+            contaDoDanilo = contaDoDaniloSilva;
+
+            Console.WriteLine("Igualdade de tipo de referência: " + (contaDoDanilo == contaDoDaniloSilva));
+
+            contaDoDanilo.saldo = 500;
+
+            Console.WriteLine(contaDoDanilo.saldo);
+            Console.WriteLine(contaDoDaniloSilva.saldo);
+        }
+    }
+}
