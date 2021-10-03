b6ee05afe57b4fff91162dd31c4f6bb18dd724a8
Danilo Silva <danilo.silva@msn.com>
2021-10-03 11:29:18 -0300

    Projeto: ByteBank - Referências Nulas

--- IntroducaoOrientacaoObjetos/05-ByteBank/Program.cs
+++ IntroducaoOrientacaoObjetos/05-ByteBank/Program.cs
@@ -6,21 +6,32 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente danilo = new Cliente();
-            danilo.nome = "Danilo";
-            danilo.profissao = "Programador";
-            danilo.cpf = "111.111.111-11";
+            // Cliente danilo = new Cliente();
+            // danilo.nome = "Danilo";
+            // danilo.profissao = "Programador";
+            // danilo.cpf = "111.111.111-11";
 
             ContaCorrente conta = new ContaCorrente();
-            conta.titular = danilo;
+            // conta.titular = danilo;
+            // conta.titular = new Cliente();
+            // conta.titular.nome = "Danilo Silva";
+            // conta.titular.cpf = "111.111.111-11";
+            // conta.titular.profissao = "Programador";
+
             conta.agencia = 863;
             conta.numero = 863452;
             conta.saldo = 500;
 
-            conta.titular.nome = "Danilo Silva";
+            if (conta.titular == null)
+            {
+                Console.WriteLine("Ops, a referência em conta titular é NULA!");
+            }
 
-            Console.WriteLine(danilo.nome);
-            Console.WriteLine(conta.titular.nome);
+            // Console.WriteLine(danilo.nome);
+            Console.WriteLine(conta.titular);
+            // Console.WriteLine(conta.titular.nome);
+            // Console.WriteLine(conta.titular.cpf);
+            // Console.WriteLine(conta.titular.profissao);
         }
     }
 }
