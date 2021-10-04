a837e7e2836b5dac51d807c288b63c75b192071f
Danilo Silva <danilo.silva@msn.com>
2021-10-04 15:57:23 -0300

    Projeto: ByteBank - Propriedades Somente Leitura

--- EntendendoExcecoes/ByteBank/ContaCorrente.cs
+++ EntendendoExcecoes/ByteBank/ContaCorrente.cs
@@ -8,25 +8,8 @@
 
         public Cliente Titular { get; set; }
 
-        public int Numero { get; set; }
-
-        private int _agencia;
-        public int Agencia
-        {
-            get
-            {
-                return _agencia;
-            }
-            set
-            {
-                if (value <= 0)
-                {
-                    return;
-                }
-
-                _agencia = value;
-            }
-        }
+        public int Numero { get; }
+        public int Agencia { get; }
 
         private double _saldo = 100;
         public double Saldo
