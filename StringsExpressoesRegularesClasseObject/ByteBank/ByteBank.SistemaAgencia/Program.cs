64b1b74bb09c468eddbbdbdc1d22e3578b43dca7
Danilo Silva <danilo.silva@msn.com>
2021-10-04 23:28:56 -0300

    Projeto: ByteBank - Imutabilidade e Substring

---
+++ StringsExpressoesRegularesClasseObject/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using Humanizer;
+using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // pagina?argumentos
+            // 012345678
+
+            string url = "pagina?argumentos";
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(7);
+            Console.WriteLine(argumentos);
+        }
+    }
+}
