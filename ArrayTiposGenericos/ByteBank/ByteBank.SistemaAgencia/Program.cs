7de616b4fcf9d06101378b1d1b80a2a01453daa2
Danilo Silva <danilo.silva@msn.com>
2021-10-05 01:08:30 -0300

    Projeto: ByteBank - Conhecendo Array

---
+++ ArrayTiposGenericos/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array de inteiros com 5 posições
+            int[] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+            Console.WriteLine(idades[4]);
+        }
+    }
+}
